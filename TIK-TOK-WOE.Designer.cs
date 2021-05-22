4feb33e8b3dbd0e51eb8b082a163547d023c5aa4
Erawr404 <bmonterozo0406@gmail.com>
2021-05-22 23:35:33 +0800

    Add project files.

---
+++ TIK-TOK-WOE.Designer.cs
@@ -0,0 +1,341 @@
+﻿
+namespace TIK_TOK_WOE
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textbox_Title = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.player_O_Score = new System.Windows.Forms.Label();
+            this.player_X_Score = new System.Windows.Forms.Label();
+            this.player_O = new System.Windows.Forms.Label();
+            this.player_X = new System.Windows.Forms.Label();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.RoyalBlue;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.textbox_Title);
+            this.panel1.ForeColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(120, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(580, 90);
+            this.panel1.TabIndex = 0;
+            // 
+            // textbox_Title
+            // 
+            this.textbox_Title.AutoSize = true;
+            this.textbox_Title.BackColor = System.Drawing.Color.RoyalBlue;
+            this.textbox_Title.Font = new System.Drawing.Font("Snap ITC", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textbox_Title.ForeColor = System.Drawing.Color.White;
+            this.textbox_Title.Location = new System.Drawing.Point(3, -2);
+            this.textbox_Title.Name = "textbox_Title";
+            this.textbox_Title.Size = new System.Drawing.Size(573, 86);
+            this.textbox_Title.TabIndex = 0;
+            this.textbox_Title.Text = "TIK-TOK-WOE";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.RoyalBlue;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.button9);
+            this.panel2.Controls.Add(this.button8);
+            this.panel2.Controls.Add(this.button7);
+            this.panel2.Controls.Add(this.button6);
+            this.panel2.Controls.Add(this.button5);
+            this.panel2.Controls.Add(this.button4);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.ForeColor = System.Drawing.Color.White;
+            this.panel2.Location = new System.Drawing.Point(70, 270);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(560, 560);
+            this.panel2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 170);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.RoyalBlue;
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel3.ForeColor = System.Drawing.Color.White;
+            this.panel3.Location = new System.Drawing.Point(12, 13);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(90, 90);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.RoyalBlue;
+            this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel4.Controls.Add(this.player_O_Score);
+            this.panel4.Controls.Add(this.player_X_Score);
+            this.panel4.Controls.Add(this.player_O);
+            this.panel4.Controls.Add(this.player_X);
+            this.panel4.ForeColor = System.Drawing.Color.White;
+            this.panel4.Location = new System.Drawing.Point(12, 109);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(439, 146);
+            this.panel4.TabIndex = 3;
+            // 
+            // player_O_Score
+            // 
+            this.player_O_Score.BackColor = System.Drawing.Color.White;
+            this.player_O_Score.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.player_O_Score.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.player_O_Score.ForeColor = System.Drawing.Color.Black;
+            this.player_O_Score.Location = new System.Drawing.Point(262, 75);
+            this.player_O_Score.Name = "player_O_Score";
+            this.player_O_Score.Size = new System.Drawing.Size(138, 44);
+            this.player_O_Score.TabIndex = 4;
+            this.player_O_Score.Text = "0";
+            this.player_O_Score.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // player_X_Score
+            // 
+            this.player_X_Score.BackColor = System.Drawing.Color.White;
+            this.player_X_Score.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.player_X_Score.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.player_X_Score.ForeColor = System.Drawing.Color.Black;
+            this.player_X_Score.Location = new System.Drawing.Point(262, 9);
+            this.player_X_Score.Name = "player_X_Score";
+            this.player_X_Score.Size = new System.Drawing.Size(138, 44);
+            this.player_X_Score.TabIndex = 3;
+            this.player_X_Score.Text = "0";
+            this.player_X_Score.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // player_O
+            // 
+            this.player_O.AutoSize = true;
+            this.player_O.BackColor = System.Drawing.Color.RoyalBlue;
+            this.player_O.Font = new System.Drawing.Font("Snap ITC", 20F);
+            this.player_O.ForeColor = System.Drawing.Color.White;
+            this.player_O.Location = new System.Drawing.Point(-2, 75);
+            this.player_O.Name = "player_O";
+            this.player_O.Size = new System.Drawing.Size(246, 44);
+            this.player_O.TabIndex = 2;
+            this.player_O.Text = "PLAYER O:";
+            // 
+            // player_X
+            // 
+            this.player_X.AutoSize = true;
+            this.player_X.BackColor = System.Drawing.Color.RoyalBlue;
+            this.player_X.Font = new System.Drawing.Font("Snap ITC", 20F);
+            this.player_X.ForeColor = System.Drawing.Color.White;
+            this.player_X.Location = new System.Drawing.Point(-2, 9);
+            this.player_X.Name = "player_X";
+            this.player_X.Size = new System.Drawing.Size(247, 44);
+            this.player_X.TabIndex = 1;
+            this.player_X.Text = "PLAYER X:";
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.RoyalBlue;
+            this.panel5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel5.Controls.Add(this.button11);
+            this.panel5.Controls.Add(this.button10);
+            this.panel5.ForeColor = System.Drawing.Color.White;
+            this.panel5.Location = new System.Drawing.Point(468, 109);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(232, 146);
+            this.panel5.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(191, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(170, 170);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(371, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(170, 170);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 192);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(170, 170);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(191, 192);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(170, 170);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(371, 192);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(170, 170);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(12, 373);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(170, 170);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(191, 373);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(170, 170);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(371, 373);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(170, 170);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(6, 3);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(216, 66);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "button10";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(6, 74);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(216, 66);
+            this.button11.TabIndex = 10;
+            this.button11.Text = "button11";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.DarkCyan;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(713, 849);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "mainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TIK-TOK-WOE";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label textbox_Title;
+        private System.Windows.Forms.Label player_O_Score;
+        private System.Windows.Forms.Label player_X_Score;
+        private System.Windows.Forms.Label player_O;
+        private System.Windows.Forms.Label player_X;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button10;
+    }
+}
+
